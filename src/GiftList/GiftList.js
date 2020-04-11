acfd3675fda2fedf49a6d2f916287cdd7e892baf
Guillermo <gmo.chu92@gmail.com>
2020-04-11 19:46:32 -0400

    Dynamic lists

--- src/GiftList/GiftList.js
+++ src/GiftList/GiftList.js
@@ -5,21 +5,36 @@ import DesktopBreakPoint from '../responsive_utils/DesktopBreakPoint';
 import TabletBreakPoint from '../responsive_utils/TabletBreakPoint';
 import PhoneBreakPoint from '../responsive_utils/PhoneBreakPoint';
 
+const giftMockData = [
+    {name: "Scarf"},
+    {name: "Card"},
+    {name: "Vaccum"},
+]
 
-function GiftList() {
-    return (
-        <div className="giftListContainer">
-            <div className="giftList">
-                <GiftItem name="Scarf"/>
-                <GiftItem name="Card"/>
-                <GiftItem name="Allowance"/>
+const gifts = giftMockData.map(gift => <GiftItem name={gift.name}/>)
+
+
+class GiftList extends React.Component {
+
+    addGiftToList() {
+        console.log("Attempting to add new gift")
+    }
+
+    render() {
+        return (
+            <div className="giftListContainer">
+                <div className="giftList">
+                    {gifts}
+                </div>
+                <div className="addGiftBar">
+                    <input className="addGiftInput" type="text" name="addGift" placeholder="Add a gift..."/>
+                    <button className="addGiftIcon" onClick={this.addGiftToList}>
+                        <i className="fas fa-plus"/>
+                    </button>
+                </div>
             </div>
-            <div className="addGiftBar">
-                <input className="addGiftInput" type="text" name="addGift" placeholder="Add a gift..."/>
-                <i className="addGiftIcon fas fa-plus"/>
-            </div>
-        </div>
-    )
+        )
+    }
 }
 
 export default GiftList;
